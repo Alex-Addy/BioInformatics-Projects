5b2d001b4495038aac0d39abbcce7458b2878dae
Alex Addy <alex.addy@gmail.com>
2014-09-22 13:33:28 -0400

    Did fall_off function for a uniform fall off rate

--- project1.py
+++ project1.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python
 
 from __future__ import print_function
+from __future__ import division
 
 import random
 # set up random
 
 def fall_off(size, failure_chance):
+	"""Uniform fall off
+	
+	   Returns a positive integer if the found fall_off is <= size.
+	   Returns 0 otherwise
+	"""
+	assert 1 >= failure_chance >= 0, "Failure chance is outside percent range: %r" % failure_chance
 	#let fall off rate be 5%
 	#let size of gene be 200
 	#find out if it falls off
-	#100/5 == 20
-	#200*20 == 4000
+	#20/0.05 == 4000
 	#get random number in range 1-4000
 	#if it is less than 200 then it falls off
 	#len(gene) * (100 / (fall off rate * 100)) == top of range
 	# TODO use non-normal distribution
-	pass
+	top_range = size / failure_chance
+	fallen = random.randint(1, top_range)
+	if fallen <= size: return fallen
+	else: return 0
 
 def simulate(dna, primer): # any others?
 	# loop over # of cycles
