9dcc8bc25a06646a14606806bdc42e31827c4cf2
swsoe <npt1@zips.uakron.edu>
2014-09-29 18:58:12 -0400

    Initial implementation of global_alignment

---
+++ project2.py
@@ -0,0 +1,97 @@
+def global_alignment(dna1, dna2):
+    dna1 = dna1.lower()
+    dna2 = dna2.lower()
+
+    dna1 = ' ' + dna1
+    dna2 = ' ' + dna2
+    
+    lengthTop = len(dna1)
+    lengthBot = len(dna2)
+
+    table = [[0 for x in xrange(lengthTop)] for x in xrange(lengthBot)]
+    direction = [["" for x in xrange(lengthTop)] for x in xrange(lengthBot)]
+
+    #setup table
+    #TODO: replace -2 with argument g
+    for x in range(0,lengthTop):
+        table[0][x] = x * -2
+        direction[0][x] = "left"
+    for y in range(0,lengthBot):
+        table[y][0] = y * -2
+        direction[y][0] = "up"
+
+    #table debug statement
+    #print_table(table)
+
+    #calculate values
+    for x in range(1,lengthTop):
+        for y in range(1,lengthBot):
+            up = table[y-1][x] - 2
+            left = table[y][x-1] - 2
+            if dna1[x] == dna2[y]:
+                diag = table[y-1][x-1] + 1
+            else:
+                diag = table[y-1][x-1] - 1
+
+            #selection process curently favors diagonals
+            #if a tie is present
+            table[y][x] = diag
+            direction[y][x]="diag"
+            if up > table[y][x]:
+                table[y][x] = up
+                direction[y][x]="up"
+            if left > table[y][x]:
+                table[y][x] = left
+                direction[y][x]="left"
+
+    #table debug statement
+    #print_table(table)
+    #print_table(direction)
+
+    #trace back through table
+    x = lengthTop - 1
+    y = lengthBot - 1
+
+    dnaFinal1 = ""
+    dnaFinal2 = ""
+    while not (x == 0 and y == 0):
+        if direction[y][x] == "diag":
+            dnaFinal1 = dna1[x] + dnaFinal1
+            dnaFinal2 = dna2[y] + dnaFinal2
+            x-=1
+            y-=1
+            
+        elif direction[y][x] == "up":
+            dnaFinal1 = "_" + dnaFinal1
+            dnaFinal2 = dna2[y] + dnaFinal2
+            y-=1
+
+        elif direction[y][x] == "left":
+            dnaFinal1 = dna1[x] + dnaFinal1
+            dnaFinal2 = "_" + dnaFinal2
+            x-=1
+
+    return(dnaFinal1, dnaFinal2)
+
+def print_table(table):
+    for x in range(0, len(table)):
+        print table[x]
+
+    
+
+
+if __name__ == '__main__':
+    import argparse
+    #TODO: add info
+    parser = argparse.ArgumentParser(description="TODO: add info")
+    parser.add_argument('m', type=int, help="Scoring for match")
+    parser.add_argument('i', type=int, help="Scoring for mismatch")
+    parser.add_argument('g', type=int, help="Scoring for gap")
+
+    dna1 = raw_input("Enter the first strand:")
+    dna2 = raw_input("Enter the second strand:")
+
+    strand1, strand2 =  global_alignment(dna1, dna2)
+    print strand1
+    print strand2
+    
