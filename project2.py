9edc7e41e3508d0666f381be75a1cc51c020c71f
swsoe <npt1@zips.uakron.edu>
2014-10-06 14:52:20 -0700

    Fixed direction table
    Changed direction table to use 1, 2, and 3 instead of "up", "left", and
    "diag".  Also added inserts and deletes print out and added function
    header for mutation checker.

--- project2.py
+++ project2.py
@@ -1,3 +1,7 @@
+#test case: aaac agc
+#output: _agc
+
+
 def global_alignment(dna1, dna2):
     dna1 = dna1.lower()
     dna2 = dna2.lower()
@@ -11,14 +15,20 @@ def global_alignment(dna1, dna2):
     table = [[0 for x in xrange(lengthTop)] for x in xrange(lengthBot)]
     direction = [["" for x in xrange(lengthTop)] for x in xrange(lengthBot)]
 
+    inserts = 0
+    deletes = 0
+
     #setup table
+    #1 = left
+    #2 = diag
+    #3 = up
     #TODO: replace -2 with argument g
     for x in range(0,lengthTop):
         table[0][x] = x * -2
-        direction[0][x] = "left"
+        direction[0][x] = 1
     for y in range(0,lengthBot):
         table[y][0] = y * -2
-        direction[y][0] = "up"
+        direction[y][0] = 3
 
     #table debug statement
     #print_table(table)
@@ -36,13 +46,13 @@ def global_alignment(dna1, dna2):
             #selection process curently favors diagonals
             #if a tie is present
             table[y][x] = diag
-            direction[y][x]="diag"
+            direction[y][x]=2
             if up > table[y][x]:
                 table[y][x] = up
-                direction[y][x]="up"
+                direction[y][x]=3
             if left > table[y][x]:
                 table[y][x] = left
-                direction[y][x]="left"
+                direction[y][x]=1
 
     #table debug statement
     #print_table(table)
@@ -55,21 +65,30 @@ def global_alignment(dna1, dna2):
     dnaFinal1 = ""
     dnaFinal2 = ""
     while not (x == 0 and y == 0):
-        if direction[y][x] == "diag":
+
+        #match
+        if direction[y][x] == 2:
             dnaFinal1 = dna1[x] + dnaFinal1
             dnaFinal2 = dna2[y] + dnaFinal2
             x-=1
             y-=1
             
-        elif direction[y][x] == "up":
+        #insertion
+        elif direction[y][x] == 3:
             dnaFinal1 = "_" + dnaFinal1
             dnaFinal2 = dna2[y] + dnaFinal2
             y-=1
+            inserts += 1
 
-        elif direction[y][x] == "left":
+        #deletion
+        elif direction[y][x] == 1:
             dnaFinal1 = dna1[x] + dnaFinal1
             dnaFinal2 = "_" + dnaFinal2
             x-=1
+            deletes += 1
+
+    print "Insertions: " + str(inserts)
+    print "Deletions: " + str(deletes)
 
     return(dnaFinal1, dnaFinal2)
 
@@ -77,7 +96,10 @@ def print_table(table):
     for x in range(0, len(table)):
         print table[x]
 
-    
+#TODO: write function
+def check_mutations(dna1, dna2):
+    #returns number of syn. and nonsyn. mutations
+    pass
 
 
 if __name__ == '__main__':
@@ -92,6 +114,5 @@ if __name__ == '__main__':
     dna2 = raw_input("Enter the second strand:")
 
     strand1, strand2 =  global_alignment(dna1, dna2)
-    print strand1
-    print strand2
+    print strand1 + '\n' + strand2
     
